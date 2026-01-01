23d920f1021ad0a8b6454b936ae2c2e20fff1e48
saba <sarzoo756@gmail.com>
2026-01-01 21:41:13 +0530

    short output

---
+++ Output-Based-Ques/Q6.js
@@ -0,0 +1,16 @@
+console.log(typeof NaN); // "number"
+console.log([] == false); // true       
+console.log(null == undefined); // true
+console.log(0.1 + 0.2 === 0.3); // false
+console.log(typeof []); // "object"
+console.log("5" - 2); // 3
+console.log(typeof null); // "object"
+console.log([1, 2] + [3, 4]); // "1,23,4"
+console.log(1 < 2 < 3); // true
+console.log(3 > 2 > 1); // false
+let a = (1, 2, 3);
+console.log(a);
+// 3
+console.log("saba" instanceof String); // false
+console.log([] == ![]); // true
+console.log(typeof function(){}); // "function"
